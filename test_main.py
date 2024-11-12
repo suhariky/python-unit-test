5accc9c590db0b3f5f86bfacf2cc44210772faae
Dmitry Paramonov <gamiysikdima@gmail.com>
2024-11-12 13:09:56 +0100

    update UTs

--- test_main.py
+++ test_main.py
@@ -1,18 +1,12 @@
 import unittest
-from parameterized import parameterized
 
 from main import min_of_three_vars
 
 
 class MinOfThreeVarsTestCase(unittest.TestCase):
 
-    @parameterized.expand([
-        ("negative", (1, 2, 3), 1),
-        ("integer", (2, 1, 3), 1),
-        ("large fraction", (3, 2, 1), 1),
-    ])
-    def test_min_a(self, name, input, expected):
-        self.assertEqual(min_of_three_vars(*input), expected)
+    def test_min_a(self):
+        // your test here
 
 
         
